e0030cf6e6b3ccc5c027fbc1331642302f4ccdeb
Gko <xristosgko@gmail.com>
2024-11-01 17:53:23 +0200

    added the constants of the function shuffleProgresIndicator

--- map.py
+++ map.py
@@ -1,5 +1,12 @@
 import random
+
+progressIndicators=["agriculture","architecture","economy","law","masonry","mathematics","philosophy","strategy","theology","urbanism"]
+gameProgressIndicators=[]
+
 def shuffleProgressIndicators():
     global progressIndicators,gameProgressIndicators
     random.shuffle(progressIndicators)
     gameProgressIndicators=progressIndicators[:5]
+
+
+shuffleProgressIndicators()
